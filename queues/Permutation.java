181fdc9fdace5c92546c727082f5a54df432be75
Nicolai Spohrer <nicolai@xeve.de>
2020-08-25 19:32:59 +0200

    Change permutation algorithm slightly to fulfill bonus requirements

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Iterator;
 
@@ -7,9 +8,24 @@ public class Permutation {
         int numRequested = Integer.parseInt(args[0]);
         RandomizedQueue<String> rq = new RandomizedQueue<>();
 
+        int numRead = 0;
         while (!StdIn.isEmpty()) {
             String s = StdIn.readString();
-            rq.enqueue(s);
+            ++numRead;
+            if (rq.size() < numRequested) {
+                // always add to queue if it does not have enough elements yet
+                rq.enqueue(s);
+            }
+            else {
+                // only add new element depending on random result
+                // this leads to all elements having a probability of
+                // numRequested/numRead_total of being in the queue in the end
+                if (StdRandom.uniform(0, numRead) < numRequested) {
+                    rq.dequeue();
+                    rq.enqueue(s);
+                }
+            }
+
         }
 
         // as 0 <= numRequested <= rq.size() we can do:
--- queues/README.md
+++ queues/README.md
@@ -2,4 +2,10 @@ Simple implementations of a deque and a randomized queue plus a test application
 
 See: https://coursera.cs.princeton.edu/algs4/assignments/queues/specification.php
 
-Score: 100/100
+Score: 100/100 (100.07% including bonus)
+
+Correctness:  45/45 tests passed
+
+Memory:       135/134 tests passed
+
+Timing:       193/193 tests passed
